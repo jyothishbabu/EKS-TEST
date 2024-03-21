75de3cca980ce5ddc646b27ebc08f1eade131de3
jyothishbabu <jyothishbabu@gmail.com>
2024-03-21 15:31:24 +0530

    S3 config for state

--- provider.tf
+++ provider.tf
@@ -17,15 +17,15 @@ terraform {
 
   }
 
-# backend "remote" {
-# 		hostname = "app.terraform.io"
-# 		organization = "CloudQuickLabs"
 
-# 		workspaces {
-# 			name = "AWSEKS"
-# 		}
-# 	}
-}
+
+  backend "s3" {
+    bucket                  = "jb-kanerika-terraform-state"
+    key                     = "my-terraform-project"
+    region                  = "us-east-1"
+  }
+
+
 
 
 data "aws_eks_cluster" "cluster" {
@@ -45,6 +45,7 @@ provider "kubernetes" {
 
 provider "aws" {
   region = "us-east-1"
+
 }
 
 resource "random_string" "suffix" {
@@ -55,4 +56,5 @@ resource "random_string" "suffix" {
 resource "aws_key_pair" "TestKeyPair" {
   key_name   = "TestKeyPair"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD3F6tyPEFEzV0LX3X8BsXdMsQz1x2cEikKDEY0aIj41qgxMCP/iteneqXSIFZBp5vizPvaoIR3Um9xK7PGoW8giupGn+EPuxIA4cDM4vzOqOkiMPhz5XK0whEjkVzTo4+S0puvDZuwIsdiW9mxhJc7tgBNL0cYlWSYVkz4G/fslNfRPW5mYAM49f4fhtxPb5ok4Q2Lg9dPKVHO/Bgeu5woMc7RY0p1ej6D4CKFE6lymSDJpW0YHX/wqE9+cfEauh7xZcG0q9t2ta6F6fmX0agvpFyZo8aFbXeUBr7osSCJNgvavWbM/06niWrOvYX2xwWdhXmXSrbX8ZbabVohBK41 email@example.com"
+}
 }
