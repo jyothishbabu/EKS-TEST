c31ce47f24d4c1788b2ce0ab5b0b23188add179a
jyothishbabu <jyothishbabu@gmail.com>
2024-03-21 08:54:19 +0530

    First Set of code

---
+++ outputs.tf
@@ -0,0 +1,21 @@
+
+
+output "cluster_id" {
+  description = "EKS cluster ID"
+  value       = module.eks.cluster_id
+}
+
+output "cluster_endpoint" {
+  description = "Endpoint for EKS control plane"
+  value       = module.eks.cluster_endpoint
+}
+
+
+output "cluster_name" {
+  description = "Kubernetes Cluster Name"
+  value       = module.eks.cluster_name
+}
+
+output "key_name" {
+  value = aws_key_pair.TestKeyPair
+}
